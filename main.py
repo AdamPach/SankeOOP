466115882842a576b6bce5ecd42b4da82f364021
AdamPach <adampach9@gmail.com>
2021-02-13 10:39:54 +0100

    Pohyb hada

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ WINDOW = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("PySnake!")
 
 
-SIZE_OF_BLOCKS = 30
+SIZE_OF_BLOCKS = 20
 
 BLOCK_WIDTH = WINDOW_WIDTH // SIZE_OF_BLOCKS
 BLOCK_HEIGHT = WINDOW_HEIGHT // SIZE_OF_BLOCKS
@@ -20,6 +20,8 @@ FPS = 4
 LIGHT_GREEN = (105, 209, 84)
 DARK_GREEN = (45, 117, 30)
 BLUE = (43, 122, 186)
+
+SNAKE_COLIDE_WALL = pygame.USEREVENT + 1
 
 class Game:
 
@@ -40,9 +42,28 @@ class Game:
                         self.__run = False
                     elif event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
                         self.__run = False
+                    elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:   #NAHORU
+                        self.__snake.zmenSmerHada(0, -SIZE_OF_BLOCKS)
+                    elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:   #DOLU
+                        self.__snake.zmenSmerHada(0, SIZE_OF_BLOCKS)
+                    elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:   #DOLEVA
+                        self.__snake.zmenSmerHada(-SIZE_OF_BLOCKS, 0)
+                    elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:   #DOPRAVA
+                        self.__snake.zmenSmerHada(SIZE_OF_BLOCKS, 0)
+                lastPartOfSnake = self.__snake.returnLastPartOfSnake()
+                self.__snake.snakeMovement()
+                for event in pygame.event.get():
+                    if event.type == SNAKE_COLIDE_WALL:
+                        self.__zivot = False
                 self.__draw()
             else:
-                pass
+                clock.tick(FPS)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.__run = False
+                    elif event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+                        self.__run = False
+        pygame.quit()
 
     def __draw(self):
         self.__draw_playground()
@@ -66,10 +87,34 @@ class Snake:
 
     def __init__(self):
         self.__snake = [pygame.Rect(WINDOW_WIDTH // 2,WINDOW_HEIGHT // 2,SIZE_OF_BLOCKS, SIZE_OF_BLOCKS),pygame.Rect(WINDOW_WIDTH // 2 - SIZE_OF_BLOCKS,WINDOW_HEIGHT // 2,SIZE_OF_BLOCKS, SIZE_OF_BLOCKS),pygame.Rect(WINDOW_WIDTH // 2 - SIZE_OF_BLOCKS * 2,WINDOW_HEIGHT // 2,SIZE_OF_BLOCKS, SIZE_OF_BLOCKS)]
-
+        self.__smerHada_x = 0
+        self.__smerHada_y = 0
 
     def returnSnake(self):
         return self.__snake
+
+    def zmenSmerHada(self, smerHada_x, smerHada_y):
+        self.__smerHada_x = smerHada_x
+        self.__smerHada_y = smerHada_y
+
+    def snakeMovement(self):
+        if self.__smerHada_x == 0 and self.__smerHada_y == 0:
+            pass
+        else:
+            if self.__snake[0].x + self.__smerHada_x >= WINDOW_WIDTH or self.__snake[0].x + self.__smerHada_x < 0:
+                pygame.event.post(pygame.event.Event(SNAKE_COLIDE_WALL))
+            else:
+                if self.__snake[0].y + self.__smerHada_y >= WINDOW_HEIGHT or self.__snake[0].y + self.__smerHada_y < 0:
+                    pygame.event.post(pygame.event.Event(SNAKE_COLIDE_WALL))
+                else:
+                    snakeCopy = self.__snake[:-1]
+                    snakeCopy.insert(0, pygame.Rect(snakeCopy[0].x + self.__smerHada_x, snakeCopy[0].y + self.__smerHada_y, SIZE_OF_BLOCKS, SIZE_OF_BLOCKS))
+                    self.__snake = snakeCopy
+
+    def returnLastPartOfSnake(self):
+        snakeLength = len(self.__snake)
+        lastPartOfSnake = self.__snake[snakeLength - 1]
+        return lastPartOfSnake
 
 class Berry:
 
