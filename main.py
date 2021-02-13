cfc4141f07285c2c18eaf60e4c40bfa87b2d6995
AdamPach <adampach9@gmail.com>
2021-02-13 09:05:48 +0100

    Navrh Objektu

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from random import randint
+import os
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+
+    def playGame(self):
+        pass
+
+class Snake:
+
+    def __init__(self):
+        pass
+
+
+class Berry:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.playGame()
